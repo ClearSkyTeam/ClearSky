b88c3644d0bb882e425016536360ad4577d235e2
Mr. Piggy <mcpepig123@gmail.com>
2016-01-12 17:01:27 -0800

    Zombies and Zombie villagers have different ids!

--- src/pocketmine/entity/ZombieVillager.php
+++ src/pocketmine/entity/ZombieVillager.php
@@ -2,11 +2,12 @@
 namespace pocketmine\entity;
 
 
-use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\Item as drp;
 use pocketmine\Player;
 
 class ZombieVillager extends Zombie{
+	const NETWORK_ID = 44;
+	
 	public $width = 1.031;
 	public $length = 0.891;
 	public $height = 2.125;
@@ -22,7 +23,7 @@ class ZombieVillager extends Zombie{
 
 	public function spawnTo(Player $player){
 		$pk = $this->addEntityDataPacket($player);
-		$pk->type = Zombie::NETWORK_ID;
+		$pk->type = ZombieVillager::NETWORK_ID;
 
 		$player->dataPacket($pk);
 		parent::spawnTo($player);
