b4cfe2b720555ba5f05286bdf0092ab37e604057
thebigsmileXD <thebigsmilexd@gmail.com>
2016-01-18 10:56:22 +0100

    Minecart class

--- src/pocketmine/entity/Minecart.php
+++ src/pocketmine/entity/Minecart.php
@@ -1,4 +1,51 @@
 <?php
 namespace pocketmine\entity;
 
-class Minecart extends Vehicle{}
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\network\protocol\EntityEventPacket;
+use pocketmine\item\Item as ItemItem;
+
+class Minecart extends Vehicle{
+	const NETWORK_ID = 84;
+
+	public function spawnTo(Player $player){
+		$pk = $this->addEntityDataPacket($player);
+		$pk->type = self::NETWORK_ID;
+		
+		$player->dataPacket($pk);
+		parent::spawnTo($player);
+	}
+
+	public function attack($damage, EntityDamageEvent $source){
+		parent::attack($damage, $source);
+
+		if(!$source->isCancelled()){
+			$pk = new EntityEventPacket();
+			$pk->eid = $this->id;
+			$pk->event = EntityEventPacket::HURT_ANIMATION;
+			foreach($this->getLevel()->getPlayers() as $player){
+				$player->dataPacket($pk);
+			}
+		}
+	}
+
+	public function kill(){
+		parent::kill();
+
+		foreach($this->getDrops() as $item){
+			$this->getLevel()->dropItem($this, $item);
+		}
+	}
+
+	public function getDrops(){
+		return [
+			ItemItem::get(ItemItem::MINECART, 0, 1)
+		];
+	}
+
+	public function getSaveId(){
+		$class = new \ReflectionClass(static::class);
+		return $class->getShortName();
+	}
+}
