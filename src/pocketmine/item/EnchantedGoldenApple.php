4bc3614eae4bef3def7d41cb198629967d4c75ce
Mr. Piggy <mcpepig123@gmail.com>
2016-01-21 21:43:16 -0800

    Create EnchantedGoldenApple.php

---
+++ src/pocketmine/item/EnchantedGoldenApple.php
@@ -0,0 +1,20 @@
+<?php
+namespace pocketmine\item;
+
+use pocketmine\entity\Effect;
+
+class EnchantedGoldenApple extends Food{
+	      public $saturation = 4;
+	
+	      public function __construct($meta = 0, $count = 1){
+	              parent::__construct(self::GOLDEN_APPLE, $meta, $count, "Enchanted Golden Apple");
+	      }
+      	public function getEffects(){
+		            return [
+					              [Effect::getEffect(Effect::ABSORPTION)->setDuration(120 * 20), 1],
+					              [Effect::getEffect(Effect::REGENERATION)->setDuration(30 * 20)->setAmplifier(4), 1],
+				  	            [Effect::getEffect(Effect::FIRE_RESISTANCE)->setDuration(5 * 60 * 20), 1],
+					              [Effect::getEffect(Effect::DAMAGE_RESISTANCE)->setDuration(5 * 60 * 20), 1]]:
+				        ];
+	      }
+}
