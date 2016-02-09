b02e49209b18d72be9e4e7e91cbc30c4339b0c5f
thebigsmileXD <thebigsmilexd@gmail.com>
2016-02-09 21:09:18 +0100

    Fixes RakLib block

--- src/pocketmine/block/SignPost.php
+++ src/pocketmine/block/SignPost.php
@@ -5,6 +5,7 @@ use pocketmine\item\Item;
 use pocketmine\item\Tool;
 use pocketmine\level\Level;
 use pocketmine\Player;
+use pocketmine\math\Vector3;
 
 class SignPost extends Transparent{
 
@@ -57,7 +58,7 @@ class SignPost extends Transparent{
 
 	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_NORMAL){
-			if($this->getSide(Vector3::SIDE_DOWN)->getId() === Item::AIR){
+			if($this->getSide(Vector3::SIDE_DOWN)->getId() === Block::AIR){
 				$this->getLevel()->useBreakOn($this);
 				return Level::BLOCK_UPDATE_NORMAL;
 			}
