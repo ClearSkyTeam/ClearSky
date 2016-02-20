882fcd2f87508a4a8fe52ef4c30d18e89d2e693a
Aodzip <aodzip@gmail.com>
2016-02-20 12:07:11 +0800

    Fix start.cmd

--- start.cmd
+++ start.cmd
@@ -9,7 +9,7 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist PocketMine-MP.phar (
+if exist ClearSky.phar (
 	set POCKETMINE_FILE=ClearSky.phar
 ) else (
 	if exist src\pocketmine\PocketMine.php (
