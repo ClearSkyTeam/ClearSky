3d8dbee22ac4578b1a6628c228b44a534896e1fe
thebigsmileXD <thebigsmilexd@gmail.com>
2016-02-21 09:34:13 +0100

    Slime block, untested

---
+++ src/pocketmine/block/Slimeblock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace pocketmine\block;
+
+use pocketmine\event\entity\EntityDamageEvent;
+
+class Slimeblock extends Transparent{
+	protected $id = self::SLIMEBLOCK;
+
+	public function __construct(){}
+
+	public function getName(){
+		return "Slime Block";
+	}
+
+	public function getHardness(){
+		return 0;
+	}
+
+	public function damageHandler(EntityDamageEvent $event){
+		$cause = $event->getCause();
+		$entity = $event->getEntity();
+		if($cause == EntityDamageEvent::CAUSE_FALL && $entity->getLevel()->getBlock($entity->getSide(0))->getId() === $this->getId()){
+			if($entity->isSneaking() === false) $event->setCancelled(true);
+			if(!$entity->getMotion()->distanceSquared($entity->getPosition()->subtract(0, 1)->floor()) > 0.1){
+				$entity->setMotion($entity->getMotion()->add(0, (($entity->getMotion()->getY() * 2) * 0.88), 0));
+			}
+		}
+	}
+}
