846421d97bd955d7b8a258178d580e9714f266cf
thebigsmileXD <thebigsmilexd@gmail.com>
2016-02-22 17:11:20 +0100

    Fix Snake killing

--- src/pocketmine/entity/Snake.php
+++ src/pocketmine/entity/Snake.php
@@ -2,14 +2,11 @@
 
 namespace pocketmine\entity;
 
-use pocketmine\event\entity\EntityDeathEvent;
-
 class Snake extends Entity{
 	// Have fun guessing why
 	public function kill(){
 		parent::kill();
-		$this->server->getPluginManager()->callEvent($ev = new EntityDeathEvent($this, $this->getDrops()));
-		foreach($ev->getDrops() as $item){
+		foreach($this->getDrops() as $item){
 			$this->getLevel()->dropItem($this, $item);
 		}
 	}
