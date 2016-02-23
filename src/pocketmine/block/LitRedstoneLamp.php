9201721b318c8095cb62884ebbb5ce49d2a8d1b0
SalmonDE <SalmonGER@users.noreply.github.com>
2016-02-23 16:43:51 +0100

    Fix LitRedstoneLamp.php Crash

--- src/pocketmine/block/LitRedstoneLamp.php
+++ src/pocketmine/block/LitRedstoneLamp.php
@@ -51,10 +51,6 @@ class LitRedstoneLamp extends Solid implements Redstone,RedstoneConsumer,LightSo
 	public function getHardness(){
 		return 0.3;
 	}
-	
-	public function getLightLevel(){
-		return 15;
-	}
 
 	public function getDrops(Item $item){
 		$drops = [];
