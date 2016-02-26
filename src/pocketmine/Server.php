5806080db8284ae5b1bc41593078d9d8edc0b00d
thebigsmileXD <thebigsmilexd@gmail.com>
2016-02-26 20:41:36 +0100

    @robske110 experienced spam that utf8 isnt found

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1494,9 +1494,9 @@ class Server{
 		if(!file_exists($this->filePath . "src/pocketmine/resources/".$language.".json")){
 			$language = "eng";
 		}
-		//For json error debug
-		//file_put_contents($this->dataPath . "debug.json",str_replace("\n", '', file_get_contents($this->filePath . "src/pocketmine/resources/$language.json")));
-		$translateJson = json_decode(utf8_encode(str_replace("\n", "", str_replace("\t", "", str_replace("\r", "", str_replace("#", "", file_get_contents($this->filePath . "src/pocketmine/resources/".$language.".json")))))),true);
+			// For json error debug
+			// file_put_contents($this->dataPath . "debug.json",str_replace("\n", '', file_get_contents($this->filePath . "src/pocketmine/resources/$language.json")));
+		$translateJson = json_decode(str_replace("\n", "", str_replace("\t", "", str_replace("\r", "", str_replace("#", "", file_get_contents($this->filePath . "src/pocketmine/resources/" . $language . ".json"))))), true);
 		$translateKeys = array_keys($translateJson);
 		$translateValue = array_values($translateJson);
 		foreach($translateValue as $key => $value){
