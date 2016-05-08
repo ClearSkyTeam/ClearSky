e74e3c2f8d0f0fad7e877111ce5235edd2aa600b
thebigsmileXD <thebigsmilexd@gmail.com>
2016-05-08 10:43:15 +0200

    WIP

---
+++ src/pocketmine/level/generator/normal/biome/JungleBiome.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace pocketmine\level\generator\normal\biome;
+
+use pocketmine\block\Sapling;
+use pocketmine\block\Block;
+use pocketmine\level\generator\populator\MossStone;
+use pocketmine\level\generator\populator\Tree;
+
+class JungleBiome extends NormalBiome{
+
+	public function __construct(){
+		parent::__construct();
+
+		$trees = new Tree(Sapling::JUNGLE);
+		$trees->setBaseAmount(10);
+		$this->addPopulator($trees);
+
+		$mossStone = new MossStone();
+		$mossStone->setBaseAmount(1);
+
+		$this->addPopulator($mossStone);
+
+		$this->setElevation(63, 81);
+
+		$this->temperature = 0.05;
+		$this->rainfall = 0.8;
+
+		$this->setGroundCover([
+			Block::get(Block::PODZOL, 0),
+			Block::get(Block::PODZOL, 0),
+			Block::get(Block::MOSS_STONE, 0),
+			Block::get(Block::MOSS_STONE, 0),
+			Block::get(Block::MOSS_STONE, 0),
+		]);
+	}
+
+	public function getName(){
+		return "Jungle";
+	}
+}
