e803ccaf9fa72ad0ceb5802635d5989471162b62
thebigsmileXD <thebigsmilexd@gmail.com>
2016-05-25 20:53:16 +0200

    Test

--- src/spl/BaseClassLoader.php
+++ src/spl/BaseClassLoader.php
@@ -14,8 +14,8 @@ class BaseClassLoader extends \Threaded implements ClassLoader{
      */
     public function __construct(ClassLoader $parent = null){
         $this->parent = $parent;
-        $this->lookup = \ThreadedFactory::create();
-        $this->classes = \ThreadedFactory::create();
+        $this->lookup = ThreadedFactory::create();
+        $this->classes = ThreadedFactory::create();
     }
 
     /**
