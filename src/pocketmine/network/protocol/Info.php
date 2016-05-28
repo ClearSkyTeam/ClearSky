4d2c5644317a3f3fdbb9a20ff883cc031f9a390f
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-05-28 07:59:14 -0400

    Universal?

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -11,7 +11,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol versions
 	 */
 	const CURRENT_PROTOCOL = 70;
-	const ACCEPT_PROTOCOL = [70];
+	const ACCEPT_PROTOCOL = [45, 60, 62, 70];
 	//This is for beta 1-bla and 0.14.0-0.14.bla , I think no need to change this - Aodzip
 
         const ATTRIBUTE_PACKET = 0x60;
