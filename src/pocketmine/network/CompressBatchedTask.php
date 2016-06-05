ae4296103d2bcd6348e38210e835437aa38ad89d
thebigsmileXD <thebigsmilexd@gmail.com>
2016-06-05 14:18:53 +0200

    Test if Attributes send

--- src/pocketmine/network/CompressBatchedTask.php
+++ src/pocketmine/network/CompressBatchedTask.php
@@ -9,11 +9,11 @@ class CompressBatchedTask extends AsyncTask{
 	public $level = 7;
 	public $data;
 	public $final;
-	public $targets = [];
+	public $targets;
 
 	public function __construct($data, array $targets, $level = 7){
 		$this->data = $data;
-		$this->targets = serialize($targets);
+		$this->targets = $targets;
 		$this->level = $level;
 	}
 
@@ -27,6 +27,6 @@ class CompressBatchedTask extends AsyncTask{
 	}
 
 	public function onCompletion(Server $server){
-		$server->broadcastPacketsCallback($this->final, unserialize($this->targets));
+		$server->broadcastPacketsCallback($this->final, (array) $this->targets);
 	}
 }
