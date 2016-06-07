974f8d85bf364dde3ec9e28fdffc48f167ee8c12
thebigsmileXD <thebigsmilexd@gmail.com>
2016-06-07 19:36:26 +0200

    The Range class is too small to be an util!

--- src/pocketmine/item/enchantment/EnchantmentLevelTable.php
+++ src/pocketmine/item/enchantment/EnchantmentLevelTable.php
@@ -2,8 +2,8 @@
 
 namespace pocketmine\item\enchantment;
 
+use pocketmine\item\Armor;
 use pocketmine\item\Item;
-use pocketmine\utils\Range;
 
 class EnchantmentLevelTable{
 
@@ -12,154 +12,154 @@ class EnchantmentLevelTable{
 	public static function init(){
 		self::$map = [
 			Enchantment::TYPE_ARMOR_PROTECTION => [
-				new Range(1, 21),
-				new Range(12, 32),
-				new Range(23, 43),
-				new Range(34, 54)
+				new Array(1, 21),
+				new Array(12, 32),
+				new Array(23, 43),
+				new Array(34, 54)
 			],
 
 			Enchantment::TYPE_ARMOR_FIRE_PROTECTION => [
-				new Range(10, 22),
-				new Range(18, 30),
-				new Range(26, 38),
-				new Range(34, 46)],
+				new Array(10, 22),
+				new Array(18, 30),
+				new Array(26, 38),
+				new Array(34, 46)],
 
 			Enchantment::TYPE_ARMOR_FALL_PROTECTION => [
-				new Range(5, 12),
-				new Range(11, 21),
-				new Range(17, 27),
-				new Range(23, 33)
+				new Array(5, 12),
+				new Array(11, 21),
+				new Array(17, 27),
+				new Array(23, 33)
 			],
 
 			Enchantment::TYPE_ARMOR_EXPLOSION_PROTECTION => [
-				new Range(5, 17),
-				new Range(13, 25),
-				new Range(21, 33),
-				new Range(29, 41)
+				new Array(5, 17),
+				new Array(13, 25),
+				new Array(21, 33),
+				new Array(29, 41)
 			],
 
 			Enchantment::TYPE_ARMOR_PROJECTILE_PROTECTION => [
-				new Range(3, 18),
-				new Range(9, 24),
-				new Range(15, 30),
-				new Range(21, 36)
+				new Array(3, 18),
+				new Array(9, 24),
+				new Array(15, 30),
+				new Array(21, 36)
 			],
 
 			Enchantment::TYPE_WATER_BREATHING => [
-				new Range(10, 40),
-				new Range(20, 50),
-				new Range(30, 60)
+				new Array(10, 40),
+				new Array(20, 50),
+				new Array(30, 60)
 			],
 
 			Enchantment::TYPE_WATER_AFFINITY => [
-				new Range(10, 41)
+				new Array(10, 41)
 			],
 
 			Enchantment::TYPE_ARMOR_THORNS => [
-				new Range(10, 60),
-				new Range(30, 80),
-				new Range(50, 100)
+				new Array(10, 60),
+				new Array(30, 80),
+				new Array(50, 100)
 			],
 
 			//Weapon
 			Enchantment::TYPE_WEAPON_SHARPNESS => [
-				new Range(1, 21),
-				new Range(12, 32),
-				new Range(23, 43),
-				new Range(34, 54),
-				new Range(45, 65)
+				new Array(1, 21),
+				new Array(12, 32),
+				new Array(23, 43),
+				new Array(34, 54),
+				new Array(45, 65)
 			],
 
 			Enchantment::TYPE_WEAPON_SMITE => [
-				new Range(5, 25),
-				new Range(13, 33),
-				new Range(21, 41),
-				new Range(29, 49),
-				new Range(37, 57)
+				new Array(5, 25),
+				new Array(13, 33),
+				new Array(21, 41),
+				new Array(29, 49),
+				new Array(37, 57)
 			],
 
 			Enchantment::TYPE_WEAPON_ARTHROPODS => [
-				new Range(5, 25),
-				new Range(13, 33),
-				new Range(21, 41),
-				new Range(29, 49),
-				new Range(37, 57)
+				new Array(5, 25),
+				new Array(13, 33),
+				new Array(21, 41),
+				new Array(29, 49),
+				new Array(37, 57)
 			],
 
 			Enchantment::TYPE_WEAPON_KNOCKBACK => [
-				new Range(5, 55),
-				new Range(25, 75)
+				new Array(5, 55),
+				new Array(25, 75)
 			],
 
 			Enchantment::TYPE_WEAPON_FIRE_ASPECT => [
-				new Range(10, 60),
-				new Range(30, 80)
+				new Array(10, 60),
+				new Array(30, 80)
 			],
 
 			Enchantment::TYPE_WEAPON_LOOTING => [
-				new Range(15, 65),
-				new Range(24, 74),
-				new Range(33, 83)
+				new Array(15, 65),
+				new Array(24, 74),
+				new Array(33, 83)
 			],
 
 			//Bow
 			Enchantment::TYPE_BOW_POWER => [
-				new Range(1, 16),
-				new Range(11, 26),
-				new Range(21, 36),
-				new Range(31, 46),
-				new Range(41, 56)
+				new Array(1, 16),
+				new Array(11, 26),
+				new Array(21, 36),
+				new Array(31, 46),
+				new Array(41, 56)
 			],
 
 			Enchantment::TYPE_BOW_KNOCKBACK => [
-				new Range(12, 37),
-				new Range(32, 57)
+				new Array(12, 37),
+				new Array(32, 57)
 			],
 
 			Enchantment::TYPE_BOW_FLAME => [
-				new Range(20, 50)
+				new Array(20, 50)
 			],
 
 			Enchantment::TYPE_BOW_INFINITY => [
-				new Range(20, 50)
+				new Array(20, 50)
 			],
 
 			//Mining
 			Enchantment::TYPE_MINING_EFFICIENCY => [
-				new Range(1, 51),
-				new Range(11, 61),
-				new Range(21, 71),
-				new Range(31, 81),
-				new Range(41, 91)
+				new Array(1, 51),
+				new Array(11, 61),
+				new Array(21, 71),
+				new Array(31, 81),
+				new Array(41, 91)
 			],
 
 			Enchantment::TYPE_MINING_SILK_TOUCH => [
-				new Range(15, 65)
+				new Array(15, 65)
 			],
 
 			Enchantment::TYPE_MINING_DURABILITY => [
-				new Range(5, 55),
-				new Range(13, 63),
-				new Range(21, 71)
+				new Array(5, 55),
+				new Array(13, 63),
+				new Array(21, 71)
 			],
 
 			Enchantment::TYPE_MINING_FORTUNE => [
-				new Range(15, 55),
-				new Range(24, 74),
-				new Range(33, 83)
+				new Array(15, 55),
+				new Array(24, 74),
+				new Array(33, 83)
 			],
 
 			//Fishing
 			Enchantment::TYPE_FISHING_FORTUNE => [
-				new Range(15, 65),
-				new Range(24, 74),
-				new Range(33, 83)
+				new Array(15, 65),
+				new Array(24, 74),
+				new Array(33, 83)
 			],
 
 			Enchantment::TYPE_FISHING_LURE => [
-				new Range(15, 65),
-				new Range(24, 74),
-				new Range(33, 83)
+				new Array(15, 65),
+				new Array(24, 74),
+				new Array(33, 83)
 			]
 		];
 	}
@@ -228,7 +228,7 @@ class EnchantmentLevelTable{
 			/** @var Range $range */
 			foreach($ranges as $range) {
 	            $i++;
-	            if($range->isInRange($modifiedLevel)){
+	            if($this->isInRange($range[0], $range[1], $modifiedLevel)){
 		            $result[] = $enchantment->setLevel($i);
 	            }
             }
@@ -236,5 +236,9 @@ class EnchantmentLevelTable{
 
         return $result;
     }
+	
+	public function isInRange($min, $max, $v){
+		return $v >= $min && $v <= $max;
+	}
 
 }
