8a1f1924bafc1fdd9ebe3397e5970f04e88060e7
Tim (robske_110) <robske110@users.noreply.github.com>
2016-06-16 14:13:26 +0200

    Revert "Tells the player what version the server uses"
    This reverts commit 642f428faa8978be59363652a72ebbaf3b125b41.

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -124,7 +124,6 @@ use pocketmine\utils\TextFormat;
 use raklib\Binary;
 use pocketmine\event\player\PlayerExperienceChangeEvent;
 use pocketmine\network\protocol\InteractPacket;
-use pocketmine\network\protocol\Info;
 
 /**
  * Main class that handles networking, recovery, and packet sending to the server part
@@ -1974,12 +1973,12 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 				}
 				if(!in_array($packet->protocol1,ProtocolInfo::ACCEPT_PROTOCOL)){
 					if($packet->protocol1 < ProtocolInfo::CURRENT_PROTOCOL){
-						$message = "disconnectionScreen.outdatedClient" . "Get " . Info::CURRENT_VERSION;
+						$message = "disconnectionScreen.outdatedClient";
 						$pk = new PlayStatusPacket();
 						$pk->status = PlayStatusPacket::LOGIN_FAILED_CLIENT;
 						$this->directDataPacket($pk);
 					}else{
-						$message = "disconnectionScreen.outdatedServer" . "Get " . Info::CURRENT_VERSION;
+						$message = "disconnectionScreen.outdatedServer";
 						$pk = new PlayStatusPacket();
 						$pk->status = PlayStatusPacket::LOGIN_FAILED_SERVER;
 						$this->directDataPacket($pk);
