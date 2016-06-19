9288d262cddd01bb37131346a360b94b9686a557
Tim (robske_110) <robske110@users.noreply.github.com>
2016-06-19 07:10:11 +0200

    Fix Entity body motion/rotatation
    Including the player :)

--- src/pocketmine/network/protocol/AddPlayerPacket.php
+++ src/pocketmine/network/protocol/AddPlayerPacket.php
@@ -40,9 +40,9 @@ class AddPlayerPacket extends DataPacket{
 		$this->putFloat($this->speedX);
 		$this->putFloat($this->speedY);
 		$this->putFloat($this->speedZ);
-		$this->putFloat($this->yaw);
-		$this->putFloat($this->yaw); //TODO headrot
-		$this->putFloat($this->pitch);
+		$this->putFloat($this->yaw * 0.71111);
+		$this->putFloat($this->yaw * 0.71111); //TODO headrot
+		$this->putFloat($this->pitch * 0.7111);
 		$this->putSlot($this->item);
 
 		$meta = Binary::writeMetadata($this->metadata);
