13adf5cef6e1945eacc6629b94783382f6f20770
Tim (robske_110) <robske110@users.noreply.github.com>
2016-07-03 17:20:01 +0200

    Well ItemFrames still don't work
    but no crash anymore

--- src/pocketmine/tile/ItemFrame.php
+++ src/pocketmine/tile/ItemFrame.php
@@ -22,6 +22,9 @@ class ItemFrame extends Spawnable{
 		if(!isset($nbt->ItemDropChance)){
 			$nbt->ItemDropChance = new FloatTag("ItemDropChance", 1.0);
 		}
+		if(!isset($this->namedtag->Item)){
+			$this->setItem(Item::get(Item::AIR), false);
+		}
 
 		parent::__construct($chunk, $nbt);
 	}
@@ -69,25 +72,25 @@ class ItemFrame extends Spawnable{
 	}
 
 	public function getSpawnCompound(){
-		if($this->getItem() != NULL){
+		if($this->getItem()["id"] == 0){
 			return new CompoundTag("", [
-			new StringTag("id", Tile::ITEM_FRAME),
-			new IntTag("x", (int) $this->x),
-			new IntTag("y", (int) $this->y),
-			new IntTag("z", (int) $this->z),
-			new ShortTag("Item", $this->getItem()),
-			new ByteTag("ItemRotation", $this->getItemRotation()),
-			new FloatTag("ItemDropChance", $this->getItemDropChance())
-		]);	
+				new StringTag("id", Tile::ITEM_FRAME),
+				new IntTag("x", (int) $this->x),
+				new IntTag("y", (int) $this->y),
+				new IntTag("z", (int) $this->z),
+				new ByteTag("ItemRotation", 0),
+				new FloatTag("ItemDropChance", (float) $this->getItemDropChance())
+			]);
 		}else{
 			return new CompoundTag("", [
-			new StringTag("id", Tile::ITEM_FRAME),
-			new IntTag("x", (int) $this->x),
-			new IntTag("y", (int) $this->y),
-			new IntTag("z", (int) $this->z),
-			new ByteTag("ItemRotation", $this->getItemRotation()),
-			new FloatTag("ItemDropChance", $this->getItemDropChance())
+				new StringTag("id", Tile::ITEM_FRAME),
+				new IntTag("x", (int) $this->x),
+				new IntTag("y", (int) $this->y),
+				new IntTag("z", (int) $this->z),
+				new ShortTag("Item", Item::get($this->getItem()["id"])),
+				new ByteTag("ItemRotation", (int) $this->getItemRotation()),
+				new FloatTag("ItemDropChance", (float) $this->getItemDropChance())
+			]);
 		}
-
 	}
 }
