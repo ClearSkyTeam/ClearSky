0206a77a43142bf4dc723c3e64a451a36a19958d
Dan <thebigsmilexd@gmail.com>
2016-07-19 20:07:57 +0200

    Confirmed SkeletonHorse drops

--- src/pocketmine/entity/SkeletonHorse.php
+++ src/pocketmine/entity/SkeletonHorse.php
@@ -37,7 +37,7 @@ class SkeletonHorse extends Animal /*implements Rideable*/{
 
     public function getDrops(){
         $drops = [
-            ItemItem::get(ItemItem::BONE, 0, mt_rand(0, 2))//TODO
+            ItemItem::get(ItemItem::BONE, 0, mt_rand(0, 2))
         ];
 
         return $drops;
