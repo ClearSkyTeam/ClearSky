070971e8aad9351bce8d790b5bd49188dbac98cd
robske_110 <robskeB@gmx.de>
2016-08-01 13:04:07 +0200

    Fix #609

--- src/raklib/server/ServerHandler.php
+++ src/raklib/server/ServerHandler.php
@@ -47,7 +47,7 @@ class ServerHandler{
         $this->server->pushMainToThreadPacket($buffer);
         $this->server->shutdown();
         $this->server->synchronized(function(){
-            if ($this->server) $this->server->wait(20000);
+            if (!is_null($this->server)) $this->server->wait(20000);
         });
         $this->server->join();
     }
