cc79f8fb4124191e8cf56822a8f9d22b5331ab0e
thebigsmileXD <thebigsmilexd@gmail.com>
2016-08-08 14:06:18 +0200

    Fix server crash. client may crash

--- src/pocketmine/network/protocol/CraftingDataPacket.php
+++ src/pocketmine/network/protocol/CraftingDataPacket.php
@@ -92,8 +92,8 @@ class CraftingDataPacket extends DataPacket{
 			foreach($entry->getEnchantments() as $enchantment){
 				$stream->putInt($enchantment->getId());
 				$stream->putInt($enchantment->getLevel());
+				$stream->putString($enchantment->getRandomName());
 			}
-			$stream->putString($entry->getRandomName());
 		}
 
 		return CraftingDataPacket::ENTRY_ENCHANT_LIST;
