2092cd078238dcb0f2560b12435f878c7ff23923
robske_110 <robskeB@gmx.de>
2016-08-19 15:11:14 +0200

    no longer fake light levels getBlockLightAt() is fixed therefore getFullLight() works again. Yay
    vector3

--- src/pocketmine/block/Grass.php
+++ src/pocketmine/block/Grass.php
@@ -41,19 +41,10 @@ class Grass extends Solid{
 		];
 	}
 
-	/**
-	 * @todo REMOVE THIS HACK
-	*/
-	private function fakeLightLvl($block){
-		$block->getSide(Vector3::SIDE_UP)->isSolid() ? $lightLvl = 0 : $lightLvl = 9;
-		return $lightLvl;
-	}
-
 	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_RANDOM){
 			$block = $this;
-			#$lightLvl = $this->getLevel()->getBlockLightAt($this->x, $this->y + 1, $this->z);
-			$lightLvl = $this->fakeLightLvl($block); //TODO: REMOVE THIS HACK
+			$lightLvl = $this->getLevel()->getFullLight(new Vector3($this->x, $this->y + 1, $this->z));
 			if($lightLvl < 4){
 				Server::getInstance()->getPluginManager()->callEvent($ev = new BlockSpreadEvent($block, $this, new Dirt()));
 				if(!$ev->isCancelled()){
@@ -65,7 +56,7 @@ class Grass extends Solid{
 					$y = mt_rand($this->y - 2, $this->y + 2);
 					$z = mt_rand($this->z - 1, $this->z + 1);
 					$block = $this->getLevel()->getBlock(new Vector3($x, $y, $z));
-					if($block->getId() === Block::DIRT && !$block->getSide(1) instanceof Liquid && $this->fakeLightLvl($this->getLevel()->getBlock(new Vector3($x, $y, $z))) >= 4){ //TODO: replace this hack with $this->getLevel()->getBlockLightAt($x, $y + 1, $z) >= 4
+					if($block->getId() === Block::DIRT && !$block->getSide(1) instanceof Liquid && $this->getLevel()->getFullLight(new Vector3($x, $y + 1, $z)) >= 4){
 						Server::getInstance()->getPluginManager()->callEvent($ev = new BlockSpreadEvent($block, $this, new Grass()));
 						if(!$ev->isCancelled()){
 							$this->getLevel()->setBlock($block, $ev->getNewState());
