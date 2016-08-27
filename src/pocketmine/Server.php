ccbe9f5bf3ccef4c014a0ccf1da5c8cb60b6bba1
Tux <write@imaginarycode.com>
2016-08-27 18:52:18 -0400

    Use random_bytes() instead of Utils::getRandomBytes().

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1124,7 +1124,7 @@ class Server{
 			return false;
 		}
 
-		$seed = $seed === null ? Binary::readInt(@Utils::getRandomBytes(4, false)) : (int) $seed;
+		$seed = $seed === null ? Binary::readInt(random_bytes(4)) : (int) $seed;
 
 		if(!isset($options["preset"])){
 			$options["preset"] = $this->getConfigString("generator-settings", "");
@@ -1562,7 +1562,7 @@ class Server{
 			"level-type" => "DEFAULT",
 			"enable-query" => true,
 			"enable-rcon" => false,
-			"rcon.password" => substr(base64_encode(@Utils::getRandomBytes(20, false)), 3, 10),
+			"rcon.password" => substr(base64_encode(@random_bytes(20)), 3, 10),
 			"auto-save" => true,
 		]);
 		
@@ -1653,7 +1653,6 @@ class Server{
 		}
 
 		$this->logger->info($this->getLanguage()->translateString("pocketmine.server.networkStart", [$this->getIp() === "" ? "*" : $this->getIp(), $this->getPort()]));
-		define("BOOTUP_RANDOM", @Utils::getRandomBytes(16));
 		$this->serverID = Utils::getMachineUniqueId($this->getIp() . $this->getPort());
 
 		$this->getLogger()->debug("Server unique id: " . $this->getServerUniqueId());
