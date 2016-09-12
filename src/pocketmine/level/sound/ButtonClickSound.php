9a33fdce09fa030188b505d92ecb4cf072ea5d15
thebigsmileXD <thebigsmilexd@gmail.com>
2016-09-12 08:50:25 +0200

    Formatting

--- src/pocketmine/level/sound/ButtonClickSound.php
+++ src/pocketmine/level/sound/ButtonClickSound.php
@@ -5,7 +5,7 @@ use pocketmine\math\Vector3;
 use pocketmine\network\protocol\LevelEventPacket;
 
 class ButtonClickSound extends GenericSound{
-    public function __construct(Vector3 $pos, $pitch = 1000){
-        parent::__construct($pos, LevelEventPacket::EVENT_SOUND_BUTTON_CLICK, $pitch);
-    }
+	public function __construct(Vector3 $pos, $pitch = 1000){
+		parent::__construct($pos, LevelEventPacket::EVENT_SOUND_BUTTON_CLICK, $pitch);
+	}
 }
